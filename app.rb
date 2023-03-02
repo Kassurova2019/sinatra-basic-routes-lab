435e1459cd9e4dae76335d82a9d73bed7cc19df0
Luba Kassurova <kassurova@icloud.com>
2023-03-01 23:33:33 -0500

    commit tree 4ff39418302e441b9b4d9fdc189c6a2c31a26958

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Luba"
+    end
+
+    get '/hometown' do
+        "My hometown is Atlanta"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is TQG"
+    end
 end
